85535136b60384c3faceddb9afd904b69a356634
sup39 <sms@sup39.dev>
2023-01-29 03:47:48 +0900

    replace matrix transform by simply setting properties of J2DPrint

--- drawText.c
+++ drawText.c
@@ -25,9 +25,6 @@ typedef struct {
   char unk3[8];
 } J2DPrint;
 
-void GXLoadPosMtxImm(Mtx, int);
-void PSMTXTrans(double dx, double dy, double dz, Mtx mtx);
-void PSMTXIdentity(Mtx mtx);
 void new_J2DPrint(void *this, void *font, int x4c, int lineHeight, uint32_t *colorTop, uint32_t *colorBot);
 // color.alpha = printer->color.alpha * alphaMask/0xff
 void J2DPrint_print_alpha_va(void *printer, uint8_t alphaMask, const char *fmt, va_list args);
@@ -45,9 +42,12 @@ void drawText(DrawTextOpt *opt, const char *fmt, ...) {
   new_J2DPrint(&printer, gpSystemFont, 0, opt->fontSize, &opt->colorTop, &opt->colorBot);
   printer.fontWidth = printer.fontHeight = opt->fontSize;
 
-  // mtx = translate(x, y, 0)
-  PSMTXTrans(opt->x, opt->y, 0, mtx);
-  GXLoadPosMtxImm(mtx, 0);
+  // set coordinate
+  printer.xInt = opt->x;
+  // printer.yInt = opt->y; // (actually unused)
+  printer.xFloat = opt->x;
+  printer.yFloat = opt->y;
+  printer.zFloat = 0;
 
   // print text
   J2DPrint_print_alpha_va(&printer, 0xff, fmt, args);
@@ -55,7 +55,4 @@ void drawText(DrawTextOpt *opt, const char *fmt, ...) {
 
   // no need to delete if not allocating memory from heap
   // delete_J2DPrint(&printer, -1);
-
-  PSMTXIdentity(mtx);
-  GXLoadPosMtxImm(mtx, 0);
 }
